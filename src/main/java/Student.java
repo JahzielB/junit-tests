0c35308dafc41a42262997bd412e76b893a2a7f6
JahzielB <jahzielb@gmail.com>
2020-06-30 14:51:13 -0500

    added tests for properties/methods in student class

---
+++ src/main/java/Student.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+public class Student {
+    private long Id;
+    private String name;
+    private ArrayList<Integer> grades;
+
+    public Student(String name, long id) {
+        this.name = name;
+        this.Id = id;
+        this.grades = new ArrayList<>();
+    }
+
+    public long getId() {
+        return this.Id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void addGrade(int grade) {
+        this.grades.add(grade);
+    }
+
+    public double getGradeAverage() {
+        int counter = 0;
+        int total = 0;
+        for (Integer grade : this.grades) {
+            total += grade;
+            counter++;
+        }
+        return (double) total / counter;
+    }
+}
--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,2 +1,29 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class StudentTest {
+    Student student;
+
+    @Before
+    public void setUp() {
+        student = new Student("bob", 5);
+    }
+
+    @Test
+    public void testId() {
+        assertEquals(5, student.getId());
+    }
+
+    @Test
+    public void testName() {
+        assertEquals("bob", student.getName());
+    }
+
+    @Test
+    public void testGradeMethods() {
+        student.addGrade(20);
+        student.addGrade(50);
+        assertEquals(35, student.getGradeAverage(), 0);
+    }
 }
